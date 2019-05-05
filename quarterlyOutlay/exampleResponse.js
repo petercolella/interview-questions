1ef471a21db0e82a56e4211023dcc5cbbd609bb8
petercolella <peter.joseph.colella@gmail.com>
2019-05-04 21:51:26 -0400

    example response object in separate file

---
+++ quarterlyOutlay/exampleResponse.js
@@ -0,0 +1,92 @@
+module.exports = exampleResponse = {
+  results: [
+    {
+      name: 'EPA',
+      outlay_fy: '2019',
+      outlay_fq: '1',
+      outlay_amt: '20000',
+      budget: '35000',
+      contact: 'zinka, Ryan(LOL)'
+    },
+    {
+      name: 'USCIS',
+      outlay_fy: '2018',
+      outlay_fq: '3',
+      outlay_amt: '140000',
+      budget: '15000',
+      contact: 'whoever'
+    },
+    {
+      name: 'EPA',
+      outlay_fy: '2018',
+      outlay_fq: '3',
+      outlay_amt: '98000.375',
+      budget: '15000',
+      contact: 'whoever'
+    },
+    {
+      name: 'USCIS',
+      outlay_fy: '2019',
+      outlay_fq: '3',
+      outlay_amt: '150000',
+      budget: '15000',
+      contact: 'whoever'
+    },
+    {
+      name: 'LABOR',
+      outlay_fy: '2018',
+      outlay_fq: '2',
+      outlay_amt: '1520000.33',
+      budget: '15000',
+      contact: 'whoever'
+    },
+    {
+      name: 'INTERIOR',
+      outlay_fy: '2018',
+      outlay_fq: '1',
+      outlay_amt: '117000.875',
+      budget: '15000',
+      contact: 'whoever'
+    },
+    {
+      name: 'EDUCATION',
+      outlay_fy: '2018',
+      outlay_fq: '4',
+      outlay_amt: '43000.67',
+      budget: '15000',
+      contact: 'whoever'
+    },
+    {
+      name: 'USCIS',
+      outlay_fy: '2019',
+      outlay_fq: '3',
+      outlay_amt: '60000',
+      budget: '15000',
+      contact: 'whoever'
+    },
+    {
+      name: 'LABOR',
+      outlay_fy: '2018',
+      outlay_fq: '2',
+      outlay_amt: '75000.170',
+      budget: '15000',
+      contact: 'whoever'
+    },
+    {
+      name: 'INTERIOR',
+      outlay_fy: '2018',
+      outlay_fq: '1',
+      outlay_amt: '8300.002',
+      budget: '15000',
+      contact: 'whoever'
+    },
+    {
+      name: 'EDUCATION',
+      outlay_fy: '2018',
+      outlay_fq: '4',
+      outlay_amt: '6200.777',
+      budget: '15000',
+      contact: 'whoever'
+    }
+  ]
+};
